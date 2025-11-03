755be44bf42d1296dde0c5ad68de950ce4fb5e6a
Rafael Berger <rafaelberger.dev@gmail.com>
2025-11-03 17:03:04 -0300

    header criado e responsividade adicionada para header, navbar e sobre

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 import "../styles/navbar.css";
 
-function Navbar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <nav className="navbar_container">
-        <div className="link_container">
-          <a href="#">Inicio</a>
-          <a href="#">Sobre</a>
-          <a href="#">Catálogo</a>
-          <a href="#">Contato</a>
-        </div>
-      </nav>
-    </>
+    <nav className="navbar_container">
+      <div className={`link_container ${isOpen ? "open" : ""}`}>
+        <a href="#" onClick={toggleMenu}>
+          Inicio
+        </a>
+        <a href="#sobre" onClick={toggleMenu}>
+          Sobre
+        </a>
+        <a href="#">Catálogo</a>
+        <a href="#">Contato</a>
+      </div>
+
+      <div className="menu_icon" onClick={toggleMenu}>
+        ☰
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
